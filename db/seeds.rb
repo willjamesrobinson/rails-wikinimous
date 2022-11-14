335999c6a3312622c521915bc8cc28fe89632f72
will robinson <willjamesr@gmail.com>
2022-11-14 17:23:42 +1100

    Progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Creating 10 fake articles...'
+10.times do
+  article = Article.new(
+    title:   Faker::Mountain.name,
+    content: Faker::ChuckNorris.fact
+  )
+  article.save!
+end
+puts 'Finished!'
